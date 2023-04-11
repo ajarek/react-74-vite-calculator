83990a9c2cef849caa2330cc65eff83693f30371
Ajarek <ajarek@poczta.onet.pl>
2023-04-12 00:41:03 +0200

    added logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
+import Buttons from "./components/Buttons"
 function App() {
   return (
     <div className='calculator'>
-      <div className='buttons'>
-        <h2 id="value">120.25</h2>
-        <span id="clear">Clear</span>
-        <span>/</span>
-        <span>*</span>
-        <span>7</span>
-        <span>8</span>
-        <span>9</span>
-        <span>-</span>
-        <span>4</span>
-        <span>5</span>
-        <span>6</span>
-        <span id="plus">+</span>
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>0</span>
-        <span>00</span>
-        <span>.</span>
-        <span id="equal">=</span>
-      </div>
+     <Buttons/>
     </div>
   )
 }
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Buttons = () => {
+  const [equal, setEqual] = useState([])
+
+  const arrayButtons = [
+    'Clear',
+    '/',
+    '*',
+    '7',
+    '8',
+    '9',
+    '-',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '0',
+    '00',
+    '.',
+    '=',
+  ]
+  const addId = (el) => {
+    if (el === 'Clear') return 'clear'
+    if (el === '+') return 'plus'
+    if (el === '=') return 'equal'
+  }
+
+  const buttonAction = (e) => {
+    if (e.target.textContent === 'Clear') {
+      setEqual([])
+    } else if (e.target.textContent === '=') {
+      setEqual([eval(equal.join(''))])
+    } else if (
+      (e.target.textContent === '+' && equal.length === 0) ||
+      (e.target.textContent === '-' && equal.length === 0) ||
+      (e.target.textContent === '*' && equal.length === 0) ||
+      (e.target.textContent === '/' && equal.length === 0)
+    ) {
+      setEqual([...equal])
+    } else {
+      setEqual([...equal, e.target.textContent])
+    }
+  }
+
   return (
-    <div>Buttons</div>
+    <div className='buttons'>
+      <h2 id='value'>{equal}</h2>
+      {arrayButtons.map((button) => {
+        return (
+          <span
+            onClick={buttonAction}
+            id={addId(button)}
+            key={button}
+          >
+            {button}
+          </span>
+        )
+      })}
+    </div>
   )
 }
 
